395b62e07e7d948ddeb92a69f62edaef83b0dacd
Pavel Kolomeitsev <pavelkolomeitsev1981@gmail.com>
2020-01-27 15:29:56 +0200

    update model

--- models/result.js
+++ models/result.js
@@ -1,8 +1,11 @@
 module.exports = class Result {
-    constructor(date, teams, score) {
+    constructor(date, teams, score, favTeam, homeGuest, firstHalf) {
         this.id = new Date().getTime().toString();
         this.date = date;
         this.teams = teams;
         this.score = score;
+        this.favTeam = favTeam;
+        this.homeGuest = homeGuest;
+        this.firstHalf = firstHalf;
     }
 }
