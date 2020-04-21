1eb53d0fbeb2a2762920da5e45cca652fe5d2776
Pavel Kolomeitsev <47209513+pavelkolomeitsev@users.noreply.github.com>
2020-04-21 22:27:25 +0300

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -3,6 +3,6 @@ const {Sequelize} = require('sequelize');
 
 // create a new instance of Sequelize
 // args: database`s name, user`s name, password, forth arg is an object with settings
-const sequelize = new Sequelize('basket_tables', 'root', 'My12345SQL', {dialect: 'mysql', host: 'localhost', logging: false});
+const sequelize = new Sequelize('basket_tables', 'root', 'My15SQL', {dialect: 'mysql', host: 'localhost', logging: false});
 
 module.exports = sequelize;
